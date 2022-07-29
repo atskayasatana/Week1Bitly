750465849cdba07006336cea43fc2e13daa0bb97
atskayasatana <88509622+atskayasatana@users.noreply.github.com>
2022-07-30 00:47:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import requests
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import argparse
+import sys
+
+load_dotenv()
+
+def shorten_link(token, url):
+    api_url = "https://api-ssl.bitly.com/v4/shorten"
+    payload = {"long_url": url}
+    response = requests.post(api_url, json=payload,
+                             headers=token)
+    response.raise_for_status()
+    bitlink = response.json()
+    return bitlink['link']
+
+
+def count_cliks(token, url):
+    parsed_url = urlparse(url)
+    link = f'{parsed_url.netloc}{parsed_url.path}'
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary'
+    response = requests.get(api_url, headers=token)
+    response.raise_for_status()
+    clicks_info = response.json()
+    return clicks_info['total_clicks']
+
+
+def is_bitlink(url, token):
+    parsed_url = urlparse(url)
+    link = f'{parsed_url.netloc}{parsed_url.path}'
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}'
+    response = requests.get(api_url, headers=token)
+    return response.ok
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url',nargs='?',default='http://www.reddit.com')
+    namespace=parser.parse_args(sys.argv[1:])
+
+    bitly_token = os.environ['BITLY_TOKEN']
+    headers = {"Authorization": bitly_token}
+
+    user_url = namespace.url
+    try:
+        if is_bitlink(user_url, headers):
+            print('Число переходов по ссылке:', count_cliks(headers, user_url))
+        else:
+            print('Битлинк', shorten_link(headers, user_url))
+    except(requests.exceptions.HTTPError):
+        print('Ошибка в адресе страницы')
+
+
+if __name__ == "__main__":
+    main()
