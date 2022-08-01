ff7e6c8403b1f5afe843c148be480356aca5e987
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-01 11:02:30 +0300

    Update main.py
    исправлено название переменной authorization_data

--- main.py
+++ main.py
@@ -7,31 +7,31 @@ import sys
 from pathlib import Path
 
 
-def shorten_link(authorization_data, url):
+def shorten_link(token, url):
     api_url = "https://api-ssl.bitly.com/v4/shorten"
     payload = {"long_url": url}
     response = requests.post(api_url, json=payload,
-                             headers=authorization_data)
+                             headers=token)
     response.raise_for_status()
     bitlink = response.json()
     return bitlink['link']
 
 
-def count_cliks(authorization_data, url):
+def count_cliks(token, url):
     parsed_url = urlparse(url)
     link = f'{parsed_url.netloc}{parsed_url.path}'
     api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary'
-    response = requests.get(api_url, headers=authorization_data)
+    response = requests.get(api_url, headers=token)
     response.raise_for_status()
     clicks_info = response.json()
     return clicks_info['total_clicks']
 
 
-def is_bitlink(url, authorization_data):
+def is_bitlink(url, token):
     parsed_url = urlparse(url)
     link = f'{parsed_url.netloc}{parsed_url.path}'
     api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}'
-    response = requests.get(api_url, headers=authorization_data)
+    response = requests.get(api_url, headers=token)
     return response.ok
 
 
@@ -40,14 +40,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('url', nargs='?')
     bitly_token = os.getenv('BITLY_TOKEN')
-    authorization_data = {"Authorization": bitly_token}
+    headers = {"Authorization": bitly_token}
     user_url = parser.parse_args().url
     try:
-        if is_bitlink(user_url, authorization_data):
+        if is_bitlink(user_url, headers):
             print('Число переходов по ссылке:', 
-                  count_cliks(authorization_data, user_url))
+                  count_cliks(headers, user_url))
         else:
-            print('Битлинк', shorten_link(authorization_data, user_url))
+            print('Битлинк', shorten_link(headers, user_url))
     except(requests.exceptions.HTTPError):
         print('Ошибка в адресе страницы')
 
